c608cc8963633e60b9f479998ff77d04e0839c01
yooseungyeon31 <ysy8317433@gmail.com>
2026-02-24 16:28:43 +0900

    필기 추가
    반복문 학습

--- main.cpp
+++ main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 //연산자 -> operator,산술연산자,+,-,*,/,%
 //문자열 상수 "AB"
 //문자 상수 'A','B'
-//RAM(main
+//숫자 상수 0,1,2,3,4,5,6,7,8,9,-1
+//RAM(mainMemory)
 //Money[10]
+//변수명은 파스칼 표기법 Pascal Case
+//띄어쓰기는 bsd 스타일로 적용
+//변수 생략은 노
+//money
+//    [0][1][2][3][4][5][6]
 
 int main() //entry point
 {
@@ -14,9 +20,17 @@ int main() //entry point
 	cout << 3 + 4 << endl;
 	cout << 3 * 4 << endl;*/
 
-	int Money = 10;
-	cin >> Money;
-	cout << Money;
+	//int Money[10];
+	//Money[0] = 10;
+	//Money[1] = 2;
+
+	int Money[1000];
+
+	for (int Index = 0; Index < 1000; ++Index)
+	{
+		Money[Index] = 10;
+	}
+	
 
 	return 0;
 }
