9a16d33da213da0a8d9dc150e790c4a0813d303c
yooseungyeon31 <ysy8317433@gmail.com>
2026-02-24 15:45:56 +0900

    상수
    상수 메모

--- main.cpp
+++ main.cpp
@@ -2,11 +2,21 @@
 
 using namespace std;
 
-int main()
+//연산자 -> operator,산술연산자,+,-,*,/,%
+//문자열 상수 "AB"
+//문자 상수 'A','B'
+//RAM(main
+//Money[10]
+
+int main() //entry point
 {
-	cout << "Hello, World" << endl;
+	/*cout << "Hello, World" << endl;
 	cout << 3 + 4 << endl;
-	cout << 3 * 4 << endl;
+	cout << 3 * 4 << endl;*/
+
+	int Money = 10;
+	cin >> Money;
+	cout << Money;
 
 	return 0;
 }
